9bee1be70dbbbbcd4ce04b1478b4b43762f39ef1
sorochinskkydm <sorochinskkydm@gmail.com>
2023-03-30 18:40:56 +0300

    Работа с фильтрами

--- employee-app/src/components/app/app.js
+++ employee-app/src/components/app/app.js
@@ -1,11 +1,12 @@
+import { Component } from 'react';
+
 import AppInfo from '../app-info/app-info';
-import SearchPanel from '../search-panel/seach-panel';
+import SearchPanel from '../search-panel/seach-panel'
 import AppFilter from '../app-filter/app-filter';
 import EmployeeList from '../employee-list/employee-list';
 import EmployeeAddForm from '../employee-add-form/employee-add-form';
-import './app.css';
-import { Component } from 'react';
 
+import './app.css';
 
 class App extends Component {
     constructor(props) {
@@ -15,7 +16,9 @@ class App extends Component {
                 {name: 'John C.', salary: 800, increase: false, rise: true, id: 1},
                 {name: 'Alex M.', salary: 3000, increase: true, rise: false, id: 2},
                 {name: 'Carl W.', salary: 5000, increase: false, rise: false, id: 3}
-            ]
+            ],
+            term: '', 
+            filter: 'all'
         }
         this.maxId = 4;
     }
@@ -28,13 +31,14 @@ class App extends Component {
         })
     }
 
+    // Да, пока могут добавляться пустые пользователи. Мы это еще исправим
     addItem = (name, salary) => {
         const newItem = {
             name, 
             salary,
             increase: false,
-            id: this.maxId++,
-            rise: false
+            rise: false,
+            id: this.maxId++
         }
         this.setState(({data}) => {
             const newArr = [...data, newItem];
@@ -44,37 +48,63 @@ class App extends Component {
         });
     }
 
-    onToggleProp = (id, property) => {
-        this.setState(({data}) => {
-            const index = data.findIndex(item => {
-                return item.id === id
-            });
-            const old = data[index];
-            const newItem = {...old, increase: !old.increase}
-            const newArr = [...data.slice(0,index), newItem, ...data.slice(index + 1)];
-            return {
-                data: newArr
-            }
+    onToggleProp = (id, prop) => {
+        this.setState(({data}) => ({
+            data: data.map(item => {
+                if (item.id === id) {
+                    return {...item, [prop]: !item[prop]}
+                }
+                return item;
+            })
+        }))
+    }
+
+    searchEmp = (items, term) => {
+        if (term.length === 0) {
+            return items;
+        }
+
+        return items.filter(item => {
+            return item.name.indexOf(term) > -1
         })
     }
-    
 
+    onUpdateSearch = (term) => {
+        this.setState({term});
+    }
+
+    filterPost = (items, filter) => {
+        switch (filter) {
+            case 'rise':
+                return items.filter(item => item.rise);
+            case 'moreThen1000':
+                return items.filter(item => item.salary > 1000);
+            default:
+                return items
+        }
+    }
+
+    onFilterSelect = (filter) => {
+        this.setState({filter});
+    }
 
     render() {
+        const {data, term, filter} = this.state;
         const employees = this.state.data.length;
-        const increased = this.state.data.filter(item => item.increase).length; 
+        const increased = this.state.data.filter(item => item.increase).length;
+        const visibleData = this.filterPost(this.searchEmp(data, term), filter);
+
         return (
             <div className="app">
-                <AppInfo employees={employees}
-                         increased={increased}/>
+                <AppInfo employees={employees} increased={increased}/>
     
                 <div className="search-panel">
-                    <SearchPanel/>
-                    <AppFilter/>
+                    <SearchPanel onUpdateSearch={this.onUpdateSearch}/>
+                    <AppFilter filter={filter} onFilterSelect={this.onFilterSelect}/>
                 </div>
                 
                 <EmployeeList 
-                    data={this.state.data}
+                    data={visibleData}
                     onDelete={this.deleteItem}
                     onToggleProp={this.onToggleProp}/>
                 <EmployeeAddForm onAdd={this.addItem}/>
@@ -82,6 +112,5 @@ class App extends Component {
         );
     }
 }
-
 
 export default App;
